2c6137b5ab517b6289521957aa657901a6bcde88
Mattia <matpolli93@gmail.com>
2021-03-15 16:59:57 +0100

    Added jQuery functionalities

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(function() {
-    
+    $(".right-nav li").mouseenter(function() {
+        $(this).children(".menu").addClass("active");
+    });
+
+    $(document).click(function(event){
+        $('.menu').removeClass('active');
+    });
 });
